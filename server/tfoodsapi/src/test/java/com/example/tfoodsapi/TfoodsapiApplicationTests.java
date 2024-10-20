f4983513648a2374b4736938dc037391708eaefb
nguyenquoctoan160 <115980213+nguyenquoctoan160@users.noreply.github.com>
2024-10-20 09:19:08 +0700

    init app

---
+++ server/tfoodsapi/src/test/java/com/example/tfoodsapi/TfoodsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tfoodsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfoodsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
